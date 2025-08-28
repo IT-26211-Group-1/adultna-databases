b9d63b4503a855f837e715e8777817e12731d2c2
Kurt <keduterte@gmail.com>
2025-08-28 19:57:56 +0800

    refactor: rename package from auth-db to databases and update related files; remove email verification schema and add email OTP schema

--- src/db/emailOtps.ts
+++ src/db/emailOtps.ts
@@ -1,12 +1,12 @@
 import { mysqlTable, varchar, timestamp } from "drizzle-orm/mysql-core";
 import { users } from "./users";
 
-export const emailVerifications = mysqlTable("email_verification", {
+export const emailOtps = mysqlTable("email_verification", {
   id: varchar("id", { length: 36 }).primaryKey(),
   userId: varchar("user_id", { length: 36 })
     .notNull()
     .references(() => users.id),
-  token: varchar("token", { length: 255 }).notNull().unique(),
+  otp: varchar("otp", { length: 10 }).notNull(),
   createdAt: timestamp("created_at").defaultNow(),
   expiresAt: timestamp("expires_at").notNull(),
 });
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { roles } from "./db/roles";
 export { userProfile } from "./db/userProfile";
 export { userRoles } from "./db/userRoles";
 export { users } from "./db/users";
-export { emailVerifications } from "./db/emailVerification";
+export { emailOtps } from "./db/emailOtps";
